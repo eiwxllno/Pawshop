c300a2938f8056542d30cf96215e04dc649ca55c
Erwin Llano <erwinllano95@gmail.com>
2025-07-16 06:02:47 +0800

    Update slider

--- front-page.php
+++ front-page.php
@@ -25,24 +25,16 @@ get_header();
 	</div>
 	<div class="carousel-inner">
 		<div class="carousel-item active" data-bs-interval="10000">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
-		
+		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
 		</div>
 		<div class="carousel-item" data-bs-interval="2000">
-		<img src="..." class="d-block w-100" alt="...">
-		<div class="carousel-caption d-none d-md-block">
-			<h5>Second slide label</h5>
-			<p>Some representative placeholder content for the second slide.</p>
-		</div>
+		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
 		</div>
 		<div class="carousel-item">
-		<img src="..." class="d-block w-100" alt="...">
-		<div class="carousel-caption d-none d-md-block">
-			<h5>Third slide label</h5>
-			<p>Some representative placeholder content for the third slide.</p>
-		</div>
+		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
 		</div>
 	</div>
+	
 	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 		<span class="visually-hidden">Previous</span>
