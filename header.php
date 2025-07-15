c32a3fa161df111c742f270ec31cd212d1dfdc96
Erwin Llano <erwinllano95@gmail.com>
2025-07-16 02:53:33 +0800

    Initial commit

---
+++ header.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Pawshop
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pawshop' ); ?></a>
+
+<div class="announcement-bar pt-2 pb-2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<ul class="announcement-bar__list">
+					<li >
+						<i class="bi bi-telephone rounded-circle"></i>
+						<a href="tel: +639 15 2812 6262">+639 15 2812 6262</a>
+					</li>
+					<li>
+						<i class="bi bi-envelope rounded-circle"></i>
+						<a href="mailto: @erwinllano@gmail.com">erwinllano@gmail.com</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-8 d-flex justify-content-end">
+				<ul class="announcement-bar__list">
+					<li >
+						<i class="bi bi-truck rounded-circle"></i> FREE PH Shipping
+					</li>
+					<li>
+						<i class="bi bi-clock-history rounded-circle"></i> 30-day Moneyback Guarante
+					</li>
+					<li>
+						<i class="bi bi-person rounded-circle"></i> 24/7 Customer support
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+	<header id="masthead" class="site-header">
+
+<div class="container pt-2 pb-2">
+	<div class="row align-items-center">
+		<div class="col site-header__logo">
+          <?php the_custom_logo(); ?>
+		</div>
+		<div class="col-md-5">
+			<?php aws_get_search_form( true ); ?>
+		</div>
+		<div class="col cart">
+		<a href="<?php echo wc_get_cart_url(); ?>">	<i class="bi bi-bag-dash p-2"></i></a>
+			<a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
+		</div>
+	</div>
+</div>
+
+		
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pawshop' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
