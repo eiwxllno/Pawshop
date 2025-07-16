d21c1ca0b5d82f4f38acea5a8efc9790ce422a61
Erwin Llano <erwinllano95@gmail.com>
2025-07-17 00:24:55 +0800

    Add slider

--- front-page.php
+++ front-page.php
@@ -16,37 +16,33 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-
-	<div id="carouselExampleDark" class="carousel carousel-dark slide">
-	<div class="carousel-indicators">
-		<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-		<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-		<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-	</div>
-	<div class="carousel-inner">
-		<div class="carousel-item active" data-bs-interval="10000">
-		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
+ <section class="container pb-5 pt-3">
+		<div id="carouselExampleDark" class="carousel carousel-dark slide overflow-hidden rounded">
+		   <div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		   </div>
+		 <div class="carousel-inner">
+			<div class="carousel-item active" data-bs-interval="10000">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item" data-bs-interval="2000">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-2.jpg" class="d-block w-100" alt="...">
+		  </div>
 		</div>
-		<div class="carousel-item" data-bs-interval="2000">
-		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
-		</div>
-		<div class="carousel-item">
-		  <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.png" class="d-block w-100" alt="...">
-		</div>
-	</div>
-	
-	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Previous</span>
-	</button>
-	<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Next</span>
-	</button>
-	</div>
 		
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+		</div>
+ </section>		
 </main>
 
 <?php
 
-get_footer();
+// get_footer();
