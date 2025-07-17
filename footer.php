7c53bde55aec5d473ca230d224bf82b418861ca8
Erwin Llano <erwinllano95@gmail.com>
2025-07-18 05:38:52 +0800

    Fix pages

--- footer.php
+++ footer.php
@@ -15,22 +15,22 @@
      <div class="bg-primary text-white pt-5 pb-5">
 		<div class="container">
 			<div class="row">
-				<div class="col-2">
+				<div class="col-sm-6 col-md-2">
 				 <?php dynamic_sidebar('footer-widget-col-one');?>
 				</div>
-				<div class="col-2">
+				<div class="col-sm-6 col-md-2">
                  <?php dynamic_sidebar('footer-widget-col-two');?>
 				</div>
-				<div class="col-md-4 ms-auto">
+				<div class="col-md-4 col-sm-12 ms-auto">
                  <?php dynamic_sidebar('footer-widget-col-three');?>
 				</div>
 			</div>
 		</div>
 	 </div>
-	 <div class="container pt-2 pb-2">
+	 <div class="container pt-4">
 <div class="row d-flex align-items-center">
 	<div class="col">
-		<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> develop by <a href="#" target="_blank">Erwin</a></p>
+		<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> develop by <a href="https://erwinllano.vercel.app/" target="_blank">Erwin Llano</a></p>
 	</div>
 	<div class="col h-25 d-inline-block text-end">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/payment/footer-cards.png" class="img-fluid w-50 h-50" loading="lazy" alt="...">
--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+    <div class="container pt-5">
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -30,7 +30,7 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-
+    </div>
 	</main><!-- #main -->
 
 <?php
